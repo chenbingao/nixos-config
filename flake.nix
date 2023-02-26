8bbe603a8b12bc2149a13ab137fd617b394abc16
shiro <615149633@qq.com>
2023-02-26 19:03:21 +0800

    save

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  output = { self, nixpkgs, ... }@inputs: {
+  outputs= { self, nixpkgs, ... }@inputs: {
     nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
